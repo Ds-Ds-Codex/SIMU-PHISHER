b297b8d6327e7aa958d2c0ef796a9f5036fb80b6
Ds-Ds-Codex <dark.s.m.bc@gmail.com>
2025-03-11 16:30:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://simuphisher-k7p2qsqb3-dx-hacking108s-projects.vercel.app";
+var hostURL="https://simu-phisher-lplt.onrender.com";
 //TOGGLE for Shorters
 var use1pt=false;
 
